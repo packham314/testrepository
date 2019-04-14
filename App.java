b916573dcb9dbd1833fd11b8a4e973323215f130
packham314 <49342596+packham314@users.noreply.github.com>
2019-04-14 17:26:21 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,36 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int newJeans = 45;
+		System.out.println(newJeans);
+			//new jeans cost $45
+		
+		int moneyInWallet = 50;
+		System.out.println(moneyInWallet);
+				
+		int moneyNeededIs = 50;
+		System.out.println(moneyNeededIs);
+		
+		Boolean moneyNeededis = moneyInWallet == 50;
+		System.out.println(moneyNeededis);
+			//I have enough money to buy jeans
+		
+		int moneyAfterJeansPurchase = 5;
+		System.out.println(moneyAfterJeansPurchase);
+			//I bought the jeans, now I only have $5
+		
+		String firstName = "Jarom";
+		char middleInitial = 'G';
+		String lastName = "Packham";
+		int myAgeis = 24;
+		int numberOfFriendsAt24 = 5;
+		//5 friends at 24
+		System.out.println(firstName + " " + middleInitial + " " + lastName + " " + myAgeis + " " + numberOfFriendsAt24);
+		
+			
+	}
+
+}
